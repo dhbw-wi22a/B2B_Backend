aabb9061b6107fe116b612192894fe654d7fa58e
ste-spa <steve.spatzek@gmail.com>
2024-12-04 21:41:36 +0100

    small fixes, debug = false

--- B2B_Backend/settings.py
+++ B2B_Backend/settings.py
@@ -28,7 +28,8 @@ load_dotenv(BASE_DIR / ENV_FILE)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key')  # Default value for development
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True').lower() in ('true', '1', 't')  # Check for a DEBUG environment variable
+#DEBUG = os.environ.get('DEBUG', 'True').lower() in ('true', '1', 't')  # Check for a DEBUG environment variable
+DEBUG = False
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
