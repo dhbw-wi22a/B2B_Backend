392bc0acc69df1866b6df5da13b620d7945dc761
Alan Kanjo <alan.kanjo@swan.de>
2025-01-07 20:42:08 +0100

    edit: changed urls

--- Webshop/urls.py
+++ Webshop/urls.py
@@ -3,11 +3,21 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import OrderViewSet, ItemViewSet, UserRegistrationView
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+
 router = DefaultRouter()
 router.register(r'items', ItemViewSet)
 router.register(r'orders', OrderViewSet, basename='orders')
 
 urlpatterns = [
     path('', include(router.urls)),  # Register all API routes
-    path('user/register/', UserRegistrationView.as_view(), name='user-registration')
+    path('auth/register/', UserRegistrationView.as_view(), name='user-registration'),
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/verify/', TokenVerifyView.as_view(), name='token_verify')
 ]
