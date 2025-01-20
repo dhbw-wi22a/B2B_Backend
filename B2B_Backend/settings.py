c66414d1921c725966750654a85b6e52f321f311
ste-spa <steve.spatzek@gmail.com>
2025-01-20 23:33:28 +0100

    add: swagger for easier api overview for dev

--- B2B_Backend/settings.py
+++ B2B_Backend/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'django_filters',
-    'ckeditor'
+    'ckeditor',
+    'drf_yasg'
 
 ]
 
--- Webshop/urls.py
+++ Webshop/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
 from .views import (
     OrderViewSet,
@@ -19,6 +22,19 @@ from .views import (
     CompanyGroupMembershipViewSet,
     GroupInvitationViewSet,
     ShoppingListViewSet,
+)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="B2B API",
+        default_version='v1',
+        description="API documentation for B2B Webshop",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="your_email@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 # Register ViewSets with the router
@@ -63,4 +79,7 @@ urlpatterns = [
     path('verify-email/<uuid:token>/', EmailVerificationView.as_view({'get':'verify_email'}), name="verify-email"),
     path('group/', include(group_router.urls)),  # Include the group router
     path('group/', include(group_invitation_patterns)),  # Include invitation-specific patterns
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
